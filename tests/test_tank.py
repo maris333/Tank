a4f4ac01a654a0a65cde6e1b7ce20c2952c896b2
maris333 <animaral@gmail.com>
2023-05-29 09:16:24 +0200

    Refactor tests

--- tests/test_tank.py
+++ tests/test_tank.py
@@ -1,17 +1,11 @@
-import pytest as pytest
+import pytest
 
-from src.event_sourcer import EventSourcer
 from src.tank import Tank
 from src.tank_helper import TankHelper
-from src.tank_manager import TankManager
-from src.tank_operations import TankOperations
+
 
 
 class TestTank:
-
-    @pytest.fixture
-    def tank_operations(self) -> TankOperations:
-        return TankOperations()
 
     @pytest.fixture
     def tank_helper(self) -> TankHelper:
@@ -25,49 +19,22 @@ class TestTank:
     def tank2(self, tank_helper: TankHelper) -> Tank:
         return Tank.create("tank2", 110, tank_helper)
 
-    @pytest.fixture
-    def event_sourcer(self, tank_operations: TankOperations) -> EventSourcer:
-        return EventSourcer(tank_operations)
-
-    @pytest.fixture
-    def tank_manager(self, tank_helper: TankHelper, tank_operations: TankOperations,
-                     event_sourcer: EventSourcer) -> TankManager:
-        return TankManager(tank_helper, tank_operations, event_sourcer)
-
-    def test_pour_water(self, tank1: Tank, tank_manager: TankManager):
-        tank_manager.pour_water(tank1.name, 10)
+    def test_pour_water(self, tank1: Tank):
+        tank1.pour_water(10)
         assert tank1.current_volume == 10
 
     def test_pour_water_exceeds_capacity(
-        self, tank1: Tank, tank_manager: TankManager
+            self, tank1: Tank
     ):
-        assert not tank_manager.pour_water(tank1.name, 110)
+        assert not tank1.pour_water(110)
 
-    def test_pour_out_water(self, tank1: Tank, tank_manager: TankManager):
-        tank_manager.pour_water(tank1.name, 10)
-        tank_manager.pour_out_water(tank1.name, 10)
+    def test_pour_out_water(self, tank1: Tank):
+        tank1.pour_water(10)
+        tank1.pour_out_water(10)
         assert tank1.current_volume == 0
 
     def test_pour_out_water_exceeds_current_volume(
-        self, tank1: Tank, tank_manager: TankManager
+            self, tank1: Tank
     ):
-        tank_manager.pour_water(tank1.name, 10)
+        tank1.pour_water(10)
         assert not tank1.pour_out_water(20)
-
-    def test_transfer_water(
-        self, tank1: Tank, tank2: Tank, tank_manager: TankManager
-    ):
-        tank_manager.pour_water(tank1.name, 10)
-        tank_manager.transfer_water(tank1.name, tank2.name, 10)
-        assert tank1.current_volume == 0
-        assert tank2.current_volume == 10
-
-    def test_transfer_water_tank_has_not_enough_water(
-        self, tank1: Tank, tank2: Tank, tank_manager: TankManager
-    ):
-        assert not tank_manager.transfer_water(tank1.name, tank2.name, 20)
-
-    def test_transfer_water_tank1_capacity_exceeded(
-        self, tank1: Tank, tank2: Tank, tank_manager: TankManager
-    ):
-        assert not tank_manager.transfer_water(tank1.name, tank2.name, 105)
--- tests/test_tank_helper.py
+++ tests/test_tank_helper.py
@@ -1,16 +1,10 @@
 import pytest
 
-from src.event_sourcer import EventSourcer
 from src.tank import Tank
 from src.tank_helper import TankHelper
-from src.tank_manager import TankManager
-from src.tank_operations import TankOperations
 
 
 class TestTankHelper:
-    @pytest.fixture
-    def tank_operations(self) -> TankOperations:
-        return TankOperations()
 
     @pytest.fixture
     def tank_helper(self) -> TankHelper:
@@ -29,23 +23,13 @@ class TestTankHelper:
         return Tank.create("tank3", 110, tank_helper)
 
     @pytest.fixture
-    def event_sourcer(self, tank_operations: TankOperations) -> EventSourcer:
-        return EventSourcer(tank_operations)
-
-    @pytest.fixture
-    def tank_manager(self, tank_helper: TankHelper, tank_operations: TankOperations,
-                     event_sourcer: EventSourcer) -> TankManager:
-        return TankManager(tank_helper, tank_operations, event_sourcer)
-
-    @pytest.fixture
-    def water_operations(self, tank1: Tank, tank2: Tank, tank3: Tank, tank_manager: TankManager) -> None:
-        tank_manager.pour_water(tank1.name, 100)
-        tank_manager.pour_water(tank2.name, 90)
+    def water_operations(self, tank1: Tank, tank2: Tank, tank3: Tank) -> None:
+        tank1.pour_water(100)
+        tank2.pour_water(90)
 
     def test_get_most_filled_tank(
         self,
         tank_helper: TankHelper,
-        tank_operations: TankOperations,
         water_operations: None
     ):
         for tank in tank_helper.get_most_filled_tanks():
@@ -54,7 +38,6 @@ class TestTankHelper:
     def test_get_tank_with_most_water(
         self,
         tank_helper: TankHelper,
-        tank_operations: TankOperations,
         water_operations
     ):
         for tank in tank_helper.get_tanks_with_most_water():
